1647310bbe589da36d83e6571b6ae0d2cb68cdad
Shuan <ShuanHolmes@Outlook.com>
2019-03-25 00:00:42 +0800

    Update test_SRimages_rename.py

--- NTIRE2019/OISR/src/test_SRimages_rename.py
+++ NTIRE2019/OISR/src/test_SRimages_rename.py
@@ -5,5 +5,5 @@ path = '../experiment/test/results-B100'
 for png in os.listdir(path):
     file_path = os.path.join(path, png)
     if 'x1' in png:
-        new_file_path = png.split('x')[0]
+        new_file_path = png.split('x')[0][:-1]
         os.rename(file_path, os.path.join(path, new_file_path+".png"))
